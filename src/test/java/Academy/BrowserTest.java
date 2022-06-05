0fcb38e17b266a1139c1e9007e00e74582f5c33b
seenu <seenu@SeenuvasanMani>
2022-06-05 15:54:10 +0530

    changes done

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -18,8 +18,9 @@ public class BrowserTest {
 	{
 		System.out.println("Hello Guys");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		//WebDriverManager.chromedriver().setup();
 		WebDriver driver =new ChromeDriver();
-		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
+		driver.get("http://seenucicd.azurewebsites.net/webapp/");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
 		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
